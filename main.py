b66199514a55ac520ea90ac1933f7d22dd132444
SirBaum <SirBaum@protonmail.com>
2025-03-06 01:14:33 +0100

    changed main.py for basic interaction of downloader

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import yaml
 import json
 
 # Custom imports
-from src.DatasetFile import DatasetFile
-from src.utils import dir_exists
-
+from darus import Downloader
 
 def main():
 
@@ -24,11 +22,6 @@ def main():
         f"{SERVER_URL}/api/datasets/:persistentId/?persistentId={PERSISTENT_IDENTIFIER}"
     )
 
-    if not validators.url(URL):
-        raise ValueError(
-            f"The provided url {URL} is not valid.\nPlease check your config ({CONFIG_FILE})."
-        )
-
     # Add API token if needed for authentification.
     header = None
     API_TOKEN = config["API_TOKEN"]
@@ -36,64 +29,8 @@ def main():
     if API_TOKEN:
         header = {"X-Dataverse-key": API_TOKEN}
 
-    if dir_exists(PATH):
-        try:
-            r = requests.get(URL, headers=header)
-            r.raise_for_status()
-
-            if r.ok:
-
-                json_response = json.loads(r.text)["data"]["latestVersion"]["files"]
-                dataset_files = list(
-                    map(
-                        lambda item_info: DatasetFile(
-                            item_info["dataFile"], SERVER_URL
-                        ),
-                        json_response,
-                    )
-                )
-
-                message = "Downloading:"
-
-                # Check if user wants to download only specific dataset_files
-                if FILES:
-                    dataset_files = [f for f in dataset_files if f.name in FILES]
-
-                    if len(dataset_files) != len(FILES):
-                        raise ValueError(
-                            f"Could not find all files from config ({FILES}) in response.\nPlease check your config ({CONFIG_FILE})."
-                        )
-                    message = (
-                        'Downloading custom set of files (see "FILES" config):'
-                    )
-
-                print(f'\n{message}\n{len(message)* "-"}')
-                print(
-                    *map(
-                        lambda file: f"-{file.name} [{file.get_dataset_filesize()}]",
-                        dataset_files,
-                    ),'\n',
-                    sep="\n",
-                )
-
-                if len(dataset_files) > 0:
-                    for f in dataset_files:
-                        successful = f.download(path=PATH, header=header)
-                        if successful:
-                            print(
-                                f"Download '{f.name}' successful (hash: {f.get_hash()})"
-                            )
-                        else:
-                            print(f"Error while trying to download {f.name}")
-                else:
-                    print(f"No files to download.")
-            else:
-                print(f"Error while trying to download.\n{r.reason}")
-
-        except requests.exceptions.HTTPError as err:
-            print("Error while trying to download.")
-            print(err)
-
+    dl = Downloader(URL, header, FILES)
+    dl.start_download()
 
 if __name__ == "__main__":
     main()
