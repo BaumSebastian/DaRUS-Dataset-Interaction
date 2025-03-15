6332b52f0ef967258feb1a657e9ebe2d13b55cb2
SirBaum <SirBaum@protonmail.com>
2025-03-15 03:46:42 +0100

    refactored utils and removed useless print statements.

--- darus/utils.py
+++ darus/utils.py
@@ -3,20 +3,17 @@ import os
 
 def dir_exists(path: str) -> bool:
     """
-    Checks if the specified directory exists. If not, asks the user to create it
+    Checks if the specified directory exists. If not, asks the user to create it.
 
-    :param path: The directory path to check or create
+    :param path: The path to the directory to check or create.
     :type path: str
-    :return: True if the directory exists or was created, False otherwise
+    :return: True if the directory exists or was created, False otherwise.
     :rtype: bool
     """
 
-    path_exists = True
+    path_exists = os.path.isdir(path)
 
-    if not os.path.isdir(path):
-        path_exists = False
-
-        # If directory not exists, try to create it - if the user wants to.
+    if not path_exists:
         create = (
             input(
                 f"The directory '{path}' does not exist.\nWould you like to create it? (yes[y]/no[n]): "
@@ -28,12 +25,9 @@ def dir_exists(path: str) -> bool:
         if create in ["yes", "y"]:
             try:
                 os.makedirs(path)
-                print(f"The directory '{path}' has been created.")
-                path_exists = True
-
             except Exception as e:
                 print(f"An error occurred while creating the directory: {e}")
-        else:
-            print("No directory created.")
+            else:
+                path_exists = os.path.isdir(path)
 
     return path_exists
