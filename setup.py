c21be15240f66cb613d576c93ef5735958efb988
SirBaum <SirBaum@protonmail.com>
2025-03-15 05:22:14 +0100

    removed long description as a char might caused an error in charmap

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     version="0.1.0",
     author="Sebastian Baum",
     description="Basic interaction with DaRUS",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     url="https://github.com/BaumSebastian/DaRUS-Dataset-Interaction",
     packages=["darus"],
     license="GNU",
