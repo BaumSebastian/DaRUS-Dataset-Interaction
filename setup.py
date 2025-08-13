969f4158b6f2cce38f2f1454df46eba9c7e9e71a
BaumSebastian <Sirbaum@protonmail.com>
2025-08-13 16:25:30 +0200

    changed version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = readme_file.read_text(encoding="utf-8") if readme_file.exists
 
 setup(
     name="darus",
-    version="0.2.1",  # Updated for recent CLI improvements
+    version="0.3.0",  # Added CLI interface and improved configuration
     author="Sebastian Baum",
     description="Download datasets from DaRUS (DataRepository of the University of Stuttgart)",
     long_description=long_description,
